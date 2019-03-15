f9cf9f3b8a903c561c38668260908f4db6b5f3a8
ladycandyfrogs <48605230+ladycandyfrogs@users.noreply.github.com>
2019-03-15 16:17:38 -0600

    Initial
    Start repo with hard drive files.

--- Assignment 1/ps01b.py
+++ Assignment 1/ps01b.py
@@ -3,12 +3,13 @@ portion_saved = float(input("Enter the percent of your salary to save, as a deci
 total_cost = float(input("Enter the cost of your dream home:"))
 
 portion_down_payment = 0.25
-current_savings = monthly_savings*.04
 r = 0.04
 
 monthly_savings = (annual_salary*portion_saved)/12
 months = total_cost/monthly_savings
 years = months/12
+
+current_savings = monthly_savings*.04
 
 print("Number of months:", int(months))
 print("Number of years:", "%.1f" % round(years, 1))
